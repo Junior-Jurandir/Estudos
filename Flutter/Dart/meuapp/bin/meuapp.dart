e812b8c0a1a5834a7b2ad9c37e03338425b1c841
Junior.Jurandir <Jura15012001@gmail.com>
2025-03-05 18:28:46 -0300

    Feat: Implement user input for grades and enhance approval messages

--- Flutter/Dart/meuapp/bin/meuapp.dart
+++ Flutter/Dart/meuapp/bin/meuapp.dart
@@ -1,7 +1,18 @@
+import 'dart:io';
 import 'package:meuapp/meuapp.dart' as meuapp;
 
 void main(List<String> arguments) {
-  var nota1 = 9;
-  var nota2 = 5;
+  print("Informe a primeira nota: ");
+  var line1 = stdin.readLineSync();
+  var nota1 = double.parse(line1 ?? '0');
+  print("Informe a segunda nota: ");
+  var line2 = stdin.readLineSync();
+  var nota2 = double.parse(line2 ?? '0');
+
   print(meuapp.getAprov(nota1, nota2));
+  print(meuapp.getAprovNota1(nota1));
+  print(meuapp.getAprovNota2(nota2));
+
+
+
 }
