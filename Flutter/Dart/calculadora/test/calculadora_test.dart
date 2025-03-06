11de0d598a542e7e754239eee7d9e33861316c05
Junior.Jurandir <Jura15012001@gmail.com>
2025-03-05 22:19:58 -0300

    Feat: Enhance calculator tests with addition, subtraction, multiplication, division, and unknown operation cases

--- Flutter/Dart/calculadora/test/calculadora_test.dart
+++ Flutter/Dart/calculadora/test/calculadora_test.dart
@@ -2,7 +2,23 @@ import 'package:calculadora/calculadora.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('addition', () {
+    expect(calcular(2, 3, '+'), 5);
+  });
+
+  test('subtraction', () {
+    expect(calcular(5, 3, '-'), 2);
+  });
+
+  test('multiplication', () {
+    expect(calcular(2, 3, '*'), 6);
+  });
+
+  test('division', () {
+    expect(calcular(6, 3, '/'), 2);
+  });
+
+  test('unknown operation', () {
+    expect(calcular(2, 3, '%'), 0);
   });
 }
