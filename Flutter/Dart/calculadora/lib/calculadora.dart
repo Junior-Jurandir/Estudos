7aceac193f077f13c6f7448de59b5d2cb22da4f5
Junior-Jurandir <jura15012001@gmail.com>
2025-03-16 19:29:15 -0300

    Feat: Refactor calculator operations into separate functions and streamline user input

--- Flutter/Dart/calculadora/lib/calculadora.dart
+++ Flutter/Dart/calculadora/lib/calculadora.dart
@@ -1,14 +1,35 @@
 double calcular(double num1, double num2, operacao) {
   switch (operacao) {
     case '+':
-      return num1 + num2;
+      return somar(num1, num2);
     case '-':
-      return num1 - num2;
+      return subtrair(num1, num2);
     case '*':
-      return num1 * num2;
+      return multiplicar(num1, num2);
     case '/':
-      return num1 / num2;
+      return dividir(num1, num2);
     default:
       return 0;
   }
+}
+
+Sting lerConsole(String texto) {
+  print(texto);
+  return stdin.readLineSync() ?? '0';
+}
+
+double somar(double num1, double num2) {
+  return num1 + num2;
+}
+
+double subtrair(double num1, double num2) {
+  return num1 - num2;
+}
+
+double multiplicar(double num1, double num2) {
+  return num1 * num2;
+}
+
+double dividir(double num1, double num2) {
+  return num1 / num2;
 }
